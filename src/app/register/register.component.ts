717f498bb35f1971e790bcb546c27671f48b3662
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2018-02-21 01:09:40 +0100

    Inbox decorated, not finished

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../model/user.model';
+import {NgModel} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
+
+//@Component({
+  //  selector:'register',
+    //templateUrl:'./register.component.html',
+    //styleUrls:['./register.component.css']
+//})
+
+
+
